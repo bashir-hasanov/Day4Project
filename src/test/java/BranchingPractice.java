b070f03915fd27fbe014d46c8170013eef53ed8d
Bashir Hasanov <beshir.hasanov@gmail.com>
2021-05-25 19:24:05 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        // created a new project
+        // enabled version control
+        // added .gitignore file with .idea, target and *.iml
+        // made initial commit by keeping track of all files
+        // shared project on github
     }
 
 }
